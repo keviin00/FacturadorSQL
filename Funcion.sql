08ff5b9a080023c6c97bf102000f26bbf24eb897
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-08-24 11:14:34 -0300

    Add files via upload

--- Funcion.sql
+++ Funcion.sql
@@ -1,13 +1,13 @@
 	-- FUNCION I
 use facturadorbk ;
-DROP FUNCTION IF EXISTS FN_LocalidClienteBK ;
+DROP FUNCTION IF EXISTS FN_LocalidClienteBK ; -- BORRAMOS EL FN SI EXISTE 
 delimiter //
-create function FN_LocalidClienteBK   (NOMBRE CHAR(255) )  returns CHAR (255)
+create function FN_LocalidClienteBK   (NOMBRE CHAR(255) )   RETURNS INT--  CREAMOS EL FN PARA VER CUANTAS PERSONAS VIVEN EN LA MISMA LOCALIDAD
 deterministic
 begin
 	DECLARE  NUMERO INT ;
-	select count(*)  into Numero from clientebk 
-	WHERE localidad LIKE CONCAT('%', NOMBRE , '%') ;
+	select count(*)  into Numero from clientebk -- BUSCAMOS EN LA TABLA CLIENTE BK
+	WHERE localidad LIKE CONCAT('%', NOMBRE , '%') ; -- CUANTAS PERSONAS VIVEN EN ESA LOCALIDAD INGRESADA
 	RETURN NUMERO ;
 end //
 delimiter ;
@@ -16,58 +16,57 @@ select FN_LocalidClienteBK  ('Caseros');
 		
         
        -- FUNCION II
-use facturadorbk ;
-DROP FUNCTION IF EXISTS FN_ProvinciaClienteBK ;
+
+DROP FUNCTION IF EXISTS FN_ProvinciaClienteBK ;-- BORRAMOS EL FN SI EXISTE 
 delimiter //
-create function FN_ProvinciaClienteBK (NOMBRE CHAR(255) )  returns CHAR (255)
+create function FN_ProvinciaClienteBK (NOMBRE CHAR(255) ) RETURNS INT --  CREAMOS EL FN PARA VER CUANTAS PERSONAS VIVEN EN LA MISMA PROVINCIA
 deterministic
 begin
 	DECLARE  NUMERO INT;
-	select count(*) into Numero from clientebk 
-	WHERE provincia LIKE CONCAT('%', NOMBRE , '%');
+	select count(*) into Numero from clientebk  -- BUSCAMOS EN LA TABLA CLIENTE BK
+	WHERE provincia LIKE CONCAT('%', NOMBRE , '%'); -- CUANTAS PERSONAS VIVEN EN ESA PROVINCIA INGRESADA
 	RETURN NUMERO;
 end //
 delimiter ;
 select FN_ProvinciaClienteBK ('Tierra del fuego');
 
      --   FUNCION III
-use facturadorbk ;
-DROP FUNCTION IF EXISTS FN_Productos_ModeloBK ;
+
+DROP FUNCTION IF EXISTS FN_Productos_ModeloBK ; -- BORRAMOS EL FN SI EXISTE 
 delimiter //
-create function FN_Productos_ModeloBK   (PRODUCTOS CHAR(255) )  returns CHAR (255)
+create function FN_Productos_ModeloBK   (PRODUCTOS CHAR(255) )  returns CHAR (255) --  CREAMOS EL FN PARA VER CUANTOS PRODUCTOS SON DEL MISMO MODELO
 deterministic
 begin
 	DECLARE  NUMERO INT ;
-	select count(*)into NUMERO from productobk 
-	WHERE MODELO LIKE CONCAT('%', PRODUCTOS, '%') ;
+	select count(*)into NUMERO from productobk  -- BUSCAMOS EN LA TABLA PRODUCTOSBK
+	WHERE MODELO LIKE CONCAT('%', PRODUCTOS, '%') ;  -- BUSCAMOS CUANTOS PRODUCTOS SON DEL MISMO MODELO INGRESADO POR PARAMETRO
 	RETURN NUMERO ;
 end //
 delimiter ;
 select FN_Productos_ModeloBK ('iphone');
 
 		-- FUNCION IV
-use facturadorbk ;
-DROP FUNCTION IF EXISTS FN_Productos_MarcaBK ;
+
+DROP FUNCTION IF EXISTS FN_Productos_MarcaBK ; -- BORRAMOS EL FN SI EXISTE 
 delimiter //
-create function FN_Productos_MarcaBK   (PRODUCTOS CHAR(255) )  returns CHAR (255)
+create function FN_Productos_MarcaBK   (PRODUCTOS CHAR(255) )  returns CHAR (255) --  CREAMOS EL FN PARA VER CUANTOS PRODUCTOS SON DE LA MISMA MARCA
 deterministic
 begin
 	DECLARE  NUMERO INT ;
-	select count(*)into NUMERO from productobk 
-	WHERE MARCA LIKE CONCAT('%', PRODUCTOS, '%') ;
+	select count(*)into NUMERO from productobk  -- BUSCAMOS EN LA TABLA PRODUCTOSBK
+	WHERE MARCA LIKE CONCAT('%', PRODUCTOS, '%') ; -- BUSCAMOS CUANTOS PRODUCTOS SON DE LA MISMA MARCA INGRESADO POR PARAMETRO
 	RETURN NUMERO ;
 end //
 delimiter ;
 select FN_Productos_MarcaBK ('celular');
 
 		-- FUNCION V
- use facturadorbk;
  
-DROP FUNCTION IF EXISTS FN_Productos_DisponibleBK ;
+DROP FUNCTION IF EXISTS FN_Productos_DisponibleBK ; -- BORRAMOS EL FN SI EXISTE 
 
 delimiter //
 
-create function FN_Productos_DisponibleBK (f_MARCA char(255), f_MODELO char(255)) returns int
+create function FN_Productos_DisponibleBK (f_MARCA char(255), f_MODELO char(255)) returns int --  CREAMOS EL FN PARA VER CUANTOS PRODUCTOS QUEDAN DISPONIBLE EN EL STOCK 
 
 deterministic
 
@@ -77,9 +76,9 @@ DECLARE LISTA_DE_PRODUCTOS INT ;
 
 select cantidad into lista_de_productos
 
-from stockbk inner join productobk on stockbk.ID_producto=productobk.id_producto
+from stockbk inner join productobk on stockbk.ID_producto=productobk.id_producto -- UNIMOS LAS TABLAS SOTCKBK Y PRODUCTOBK POR MEDIO DEL ID_PRODUCTO QUE ES PARTE DE LA TABLA STOCKBK
 
-where marca = f_MARCA and modelo = f_MODELO;
+where marca = f_MARCA and modelo = f_MODELO; -- VAMOS A CONSULTAR CUANTO STOCK HAY DEL PRODUCTO QUE CUMPLA CON LOS PARAMETROS MARCA Y MODELO 
 
 RETURN LISTA_DE_PRODUCTOS;
 
@@ -90,17 +89,17 @@ delimiter ;
 select FN_Productos_DisponibleBK ('Celular','Sony');
 
 		-- FUNCION VI
-use facturadorbk ;
-DROP FUNCTION IF EXISTS FN_Recibobk;
-delimiter //
-create function FN_Recibobk (PRODUCTO INT) RETURNS INT
+DROP FUNCTION IF EXISTS FN_Recibobk; -- BORRAMOS EL FN SI EXISTE 
+delimiter // 
+create function FN_Recibobk (PRODUCTO INT) RETURNS INT --  CREAMOS EL FN PARA VER CUANTAS PERSONAS COMPRARON EL MISMO PRODUCTO
 deterministic
 begin
 	DECLARE  NUMERO INT ;
-	select count(*)into NUMERO from recibobk 
-	WHERE ID_Producto = PRODUCTO ; 
+	select count(*)into NUMERO from recibobk  -- BUSCAMOS EN LA TABLA RECIBOBK
+	WHERE ID_Producto = PRODUCTO ;  -- BUSCAMOS LOS PRODUCTOS QUE TENGAN EL ID_PRODUCTO INGRESADO
 	RETURN NUMERO  ;
 end //
 delimiter ;
 select FN_Recibobk  ('4');
+
 
