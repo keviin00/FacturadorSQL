0b02b1b68ff94dd23ca8fd96fe2a6bae9b59143b
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-08-30 19:56:06 -0300

    Add files via upload

--- implementacion de sentencias.sql
+++ implementacion de sentencias.sql
@@ -1,13 +1,54 @@
  use mysql;
  use facturadorbk;
 show tables;
-select * from db;
-drop user if  exists'facturador_prueba'@'localhost'; -- borra el usuario creado si existe
-create user if not exists 'facturador_prueba'@'localhost:3306' identified by '123456' ; -- consulta si eciste el usuario sino lo crea
+-- usuario de lectura
+drop user if  exists'facturador_KEA'@'localhost'; -- borra el usuario creado si existe
+create user if not exists 'facturador_KEA'@'localhost' identified by '123456' ; -- consulta si eciste el usuario sino lo crea
 flush privileges;
-  -- rename user facturador_prueba@localhost to facturador_prueba@hostlocal ; renombrar usuarios 
-  select * from user where user like '%facturador%';
-  -- permisos
-  show grants for newuser@localhost;
-  show grants for 'facturador_prueba'@'localhost:3306';
-  show grants for root@localhost; -- usuario por defecto de sql tiene permisos para absolutamente todo
+show grants for 'facturador_KEA'@'localhost';
+ 
+grant select,update,insert on facturadorbk.clientebk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.garantiabk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.localidadesbk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_clientes to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_clientes_actualizacion_email to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_clientes_actualizacion_l to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_clientes_actualizacion_nombre to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_clientes_actualizacion_p to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.log_auditoria_delete_cliente to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.productobk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.provinciasbk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.recibobk to 'facturador_KEA'@'localhost';
+grant select on facturadorbk.stockbk to 'facturador_KEA'@'localhost';
+
+-- pruebas
+SELECT * FROM facturadorbk.clientebk;
+
+  -- rename user facturador_KEA@localhost to facturador_prueba@localhost ; renombrar usuarios 
+  -- alter user 'facturador_KEA'@'localhost' indetified by 'newpass' -- cambiamos la contraseña del usuario
+  select * from user  ;
+-- usuario de lectura,insert y modificacion 
+drop user if  exists'facturador_KEAFull'@'localhost'; -- borra el usuario creado si existe
+create user if not exists 'facturador_KEAFull'@'localhost' identified by '123456' ; -- consulta si eciste el usuario sino lo crea
+flush privileges;
+show grants for 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.clientebk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.garantiabk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.localidadesbk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_clientes to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_clientes_actualizacion_email to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_clientes_actualizacion_l to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_clientes_actualizacion_nombre to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_clientes_actualizacion_p to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.log_auditoria_delete_cliente to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.productobk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.provinciasbk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.recibobk to 'facturador_KEAFull'@'localhost';
+grant select,update,insert on facturadorbk.stockbk to 'facturador_KEAFull'@'localhost';
+
+-- pruebas
+SELECT * FROM facturadorbk.clientebk;
+INSERT INTO clientebk (nombre, apellido, email, dni, provincia, localidad) VALUES ('prueba', 'insert', 'insert.prueba@gmail.com', '123456789', 'Buenos Aires', 'caseros');
+UPDATE clientebk SET apellido = 'update' WHERE (ID_Cliente = '12');
+
+revoke update on *.* from 'facturador_KEAFull'@'localhost'; -- quitar permisos
