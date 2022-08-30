24049fd6fb2195234318f70a246ba5dc261125e6
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-08-30 18:22:01 -0300

    Add files via upload

--- Sublenguaje TCL.sql
+++ Sublenguaje TCL.sql
@@ -1,19 +1,18 @@
 use facturadorbk;
 select * from clientebk;
-
+SELECT @@AUTOCOMMIT;
+SET @@AUTOCOMMIT =1;
+SET @@AUTOCOMMIT =0;
 START TRANSACTION;
 DELETE FROM clientebk WHERE ID_cliente=10 ;
 DELETE FROM clientebk WHERE ID_cliente=11 ;
-DELETE FROM clientebk WHERE ID_cliente=12;
-DELETE FROM clientebk WHERE ID_cliente=13;
 ROLLBACK;
 COMMIT;
+
+
  -- -- --
 select * from productobk;
 START TRANSACTION;
-
-SAVEPOINT sp1;
-
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('Ryzen','AMD Ryzen™ 5000','t824JD75','250000','1');
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('msi','MAG Z590 TORPEDO','zD2hJnoQ','142269','1');
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('corsair','WATERCOOLINGICUE H150i ELITE CAPELLIX','HNXKMbbT','146659','1');
@@ -25,10 +24,10 @@ INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) V
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('Seasonic ','80 PLUS Titanium Certified','34wQTBQR','69999','1');
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('Asus ','ROG MAXIMUS Z690 HERO EVA EDITION','EDUVhbFZ','320000','1');
 INSERT INTO productobk (Marca,Modelo,Serial_number,Precio,Producto_disponible) VALUES ('NVIDIA','GeForce RTX 30 Series RTX 3060','xZWT6sDQ','142999','1');
-
 SAVEPOINT sp3;
-	ROLLBACK TO sp1;
+	
+  
 ROLLBACK TO sp2;
-	ROLLBACK TO sp3;
-RELEASE SAVEPOINT sp2;
+ROLLBACK TO sp3;
+-- RELEASE SAVEPOINT sp2;
  
